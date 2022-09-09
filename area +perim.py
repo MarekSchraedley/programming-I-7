68a71873ea78f39c287d4f87b42e18226476444e
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2022-09-09 18:42:35 -0500

    Add files via upload

---
+++ area +perim.py
@@ -0,0 +1,11 @@
+
+length = int(input("Enter the length "))
+width = int(input("Enter the width "))
+
+area = length * width
+perim = 2 * length + 2 * width
+
+print("the length is", length)
+print("the width is", width)
+print("the area is", area)
+print("the perimiter is", perim)

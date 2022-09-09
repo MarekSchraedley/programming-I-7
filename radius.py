d35592f718c2a066c08ec7acd947a6fea63afb42
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2022-09-09 18:46:42 -0500

    Add files via upload

---
+++ radius.py
@@ -0,0 +1,8 @@
+radius = 3.712
+pi = 3.14159
+circum = 2 * pi * radius
+area = radius * radius * pi
+print("the radius of the circle =", radius)
+print("the area of the circle = %.3f " % area)
+print("the circumfrence of the circle = %.3f " % circum)
+# i checked with a calculator, the answers you provided are wrong

9e8f59b6ee253b7f343fdb098acd506befcc9655
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2022-09-09 19:21:31 -0500

    Add files via upload

---
+++ average (1).py
@@ -0,0 +1,13 @@
+v1 = 475
+v2 = 821
+v3 = 369
+v4 = 562
+summ = v1 + v2 + v3 + v4
+avrg = summ / 4
+print("the sum of the four numbers is", summ)
+print("\nthe average of the four numbers is", avrg)
+#i wrote everything above this by myself. my dad showed me a quick online tutorial. (text not video.) i typed the code below using that.
+orders = [475.0, 821.0, 369.0, 562.0]
+average = sum(orders) / len(orders)
+print("second method average =" ,average)
+#It didn't work at first because I called the variable "sum" in line 5. I changed that to summ and it worked.
